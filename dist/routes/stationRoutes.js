0cebff82cb49ef3b1140842cdd6e80b90eb94437
Acme2020 <stefandrescher82@googlemail.com>
2025-09-16 22:22:53 +0200

    (root): removed dist

--- dist/routes/stationRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { autocompleteStations, getStationBoard, } from "../controllers/stationController";
-const router = Router();
-router.get("/stations/autocomplete", autocompleteStations);
-router.get("/stations/:stationId/board", getStationBoard);
-export default router;
--- dist/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import stationRoutes from "./routes/stationRoutes";
-const api = express();
-api.use(express.json());
-api.use("/api", stationRoutes);
-const PORT = process.env.PORT || 3000;
-api.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
